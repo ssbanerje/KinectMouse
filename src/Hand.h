9bb441b1b4a6c2d3ea6920b6996e742c94b43b26
Subho Sankar Banerjee <subs.zero@gmail.com>
2011-05-23 15:17:29 +0530

    hand type

--- src/Hand.h
+++ src/Hand.h
@@ -3,9 +3,14 @@
 #include "ofxVec3f.h"
 #include "Finger.h"
 
+typedef enum {LeftHand, RightHand} HandType;
+
 class Hand : public ofxVec3f {
 private:
     Finger fingers[5];
+    HandType type;
 public:
     void SetFinger(FingerType finger, int x, int y, int z);
+    HandType getType() {return type;}
+    void setType(HandType h) {type = h;}
 };
